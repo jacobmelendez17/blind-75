2f3ee8d46de93ab7945f0fd488c5f3b6e2578cec
jacobmelendez17 <nerdalert46@gmail.com>
2026-08-18 17:49:35 -0700

    191. Number of One Bits

---
+++ 191-Number-Of-One-Bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        bits = 0
+        while n != 0:
+            if(n & 1 == 1):
+                bits += 1
+            n = n >> 1
+        return bits
+
+# In this solution we look at the bit value of n and just read through that
+# We start leftmost and simply see if the bit of n is 1 or 0 and then shift to the next bit on the right
+# This one only requires bit manipulation
